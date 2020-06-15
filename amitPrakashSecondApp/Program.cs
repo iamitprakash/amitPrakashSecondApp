4325e73d4b22fc1a4a1ee8e8ce4016ef48256f56
iamitprakash <amitbcet2k15@gmil.com>
2020-06-16 01:45:10 +0530

    Initial check-in of module amitPrakashSecondApp

---
+++ amitPrakashSecondApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace amitPrakashSecondApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
